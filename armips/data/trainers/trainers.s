3a4199f57a61384862f42479af9c28921090a365
Julian Durr <43624274+jadurr@users.noreply.github.com>
2025-07-08 23:55:15 -0400

    Changed trainers.s
    Updated Trainers.s to no longer have Palafin

--- armips/data/trainers/trainers.s
+++ armips/data/trainers/trainers.s
@@ -18321,7 +18321,7 @@ trainerdata 419, "Ellen"
         ivs 255
         abilityslot 0
         level 63
-        pokemon SPECIES_PALAFIN
+        pokemon SPECIES_BAXCALIBUR
         ballseal 0
     endparty
 
