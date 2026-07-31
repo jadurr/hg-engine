81b8dd92d8f6c1bc9ec31cb2522d1978b4622f1b
Julian Durr <43624274+jadurr@users.noreply.github.com>
2026-07-31 17:07:30 -0400

    Update item.h

--- include/constants/item.h
+++ include/constants/item.h
@@ -2691,15 +2691,14 @@
 
 #define MAX_BASE_ITEM_NUM ITEM_CANARI_BREAD
 
-#define ITEM_INFINITE_RARE_CANDY (MAX_BASE_ITEM_NUM + 1)
-#define ITEM_INFINITE_REPEL (MAX_BASE_ITEM_NUM + 2)
-
-#define MAX_TOTAL_ITEM_NUM ITEM_INFINITE_REPEL
 
 // define your custom move effects below like this
 // #define ITEM_CUSTOM_1 (MAX_BASE_ITEM_NUM + 1)
 
-#define MAX_TOTAL_ITEM_NUM ITEM_CANARI_BREAD
+#define ITEM_INFINITE_RARE_CANDY (MAX_BASE_ITEM_NUM + 1)
+#define ITEM_INFINITE_REPEL (MAX_BASE_ITEM_NUM + 2)
+
+#define MAX_TOTAL_ITEM_NUM ITEM_INFINITE_REPEL
 
 
 #define NUM_MEGA_STONES (48) // includes the pixie plate, doesn't include plza megas due to overflowing bag
